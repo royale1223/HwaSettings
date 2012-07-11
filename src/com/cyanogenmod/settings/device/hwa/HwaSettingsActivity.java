68c3ca520e71dfe10d44c53480ec3bc9b70cc165
Binoy Babu <royale1223@gmail.com>
2012-07-11 19:26:08 +0530

    refactor activity to HwaSettingsActivity

--- src/com/cyanogenmod/settings/device/hwa/HwaSettingsActivity.java
+++ src/com/cyanogenmod/settings/device/hwa/HwaSettingsActivity.java
@@ -3,7 +3,7 @@ package com.cyanogenmod.settings.device.hwa;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class HwaActivity extends Activity {
+public class HwaSettingsActivity extends Activity {
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
