335e768c5b1b275308af985c2543f4e0c557fc7a
Binoy Babu <royale1223@gmail.com>
2012-07-13 05:18:58 +0530

    No need to scan twice when db is created

--- src/com/cyanogenmod/settings/device/hwa/DatabaseHelper.java
+++ src/com/cyanogenmod/settings/device/hwa/DatabaseHelper.java
@@ -30,7 +30,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		db.execSQL(DATABASE_CREATE_STATEMENT);
-		DatabaseTools.scanPackages(db, mContext);
 	}
 
 	@Override
