c7d267da0100218b51e333d5c42d262829e970fe
Binoy Babu <royale1223@gmail.com>
2012-07-15 11:45:40 +0530

    Proper NotificationUri for cusror

--- src/com/cyanogenmod/settings/device/hwa/PackageListProvider.java
+++ src/com/cyanogenmod/settings/device/hwa/PackageListProvider.java
@@ -94,9 +94,9 @@ public class PackageListProvider extends ContentProvider {
 		}
 		Cursor cursor = queryBuilder.query(mDatabase, projection, selection,
 				selectionArgs, null, null, sortOrder);
-		cursor.setNotificationUri(mContext.getContentResolver(), uri);
 		cursor = mDatabase.query(DatabaseHelper.PACKAGE_TABLE, projection,
 				selection, selectionArgs, null, null, null);
+		cursor.setNotificationUri(mContext.getContentResolver(), CONTENT_URI);
 		return cursor;
 	}
 
