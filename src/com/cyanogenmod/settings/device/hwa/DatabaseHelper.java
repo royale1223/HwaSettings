ecaab1ba7bda4b8abd84ecd077c0639aff1f47e8
Binoy Babu <royale1223@gmail.com>
2012-07-17 10:59:26 +0530

    Bump database version to 2

--- src/com/cyanogenmod/settings/device/hwa/DatabaseHelper.java
+++ src/com/cyanogenmod/settings/device/hwa/DatabaseHelper.java
@@ -4,11 +4,12 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
+import android.widget.Toast;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
 	private static final String DATABASE_NAME = "package_database";
-	private static final int DATABASE_VERSION = 1;
+	private static final int DATABASE_VERSION = 2;
 	public static final String PACKAGE_TABLE = "packages";
 	protected Context mContext;
 	private static final String DATABASE_CREATE_STATEMENT = "CREATE TABLE IF NOT EXISTS "
@@ -35,6 +36,12 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		return;
+		Toast.makeText(
+				mContext,
+				mContext.getString(R.string.hwa_settings_database_upgrade,
+						oldVersion, newVersion), Toast.LENGTH_LONG).show();
+		db.execSQL("DROP TABLE IF EXISTS " + PACKAGE_TABLE);
+		db.execSQL(DATABASE_CREATE_STATEMENT);
+		DatabaseTools.scanPackages(db, mContext);
 	}
 }
